e94ad4263dade8b849104417ea96eca85241bc41
007 <yamatsuba008@gmail.com>
2020-05-27 12:52:42 +0300

    Start create my CV

---
+++ src/homepage/homepage.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className="menu-item">
+                <div className='content'>
+                    <h1 className='title'>Photo</h1>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className='content'>
+                    <h1 className='title'>Skill</h1>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className='content'>
+                    <h1 className='title'>lincks</h1>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className='content'>
+                    <h1 className='title'>About me</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;
+ 
+
