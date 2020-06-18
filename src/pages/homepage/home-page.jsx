ffb1acca3b7dfce9c78a17d826cefa8510d2d1cc
SergiiKov <sergiikovtun.vn@gmail.com>
2020-06-18 14:09:01 +0300

    add soe data and styles to pages v2

--- src/pages/homepage/home-page.jsx
+++ src/pages/homepage/home-page.jsx
@@ -9,6 +9,13 @@ const HomePage = () => {
             <h3>A BEAUTIFUL, RESPONSIVE, REACT APP WRITTEN WITH MODERN JAVASCRIPT.</h3>
             <p>This is react website created to introduce me and my skills in web development (and my skills in english).</p>
             <p>Source available <a href='https://mdbootstrap.com/docs/jquery/css/colors/'>here.</a></p>
+                <div>
+                    <p>It's just a string</p>
+                    <p>This string randomly changes color. Gradient!</p>
+                    <p>This line will disappear after five clicks and appear in ten seconds</p>
+                    <p>This line will disappear after two clicks and reappear in one hour. Do not try to update the site it will not help!</p>
+                    <p>I said it wouldn't help</p>
+                </div>
         </div>
         );
     };
--- src/pages/resume/resume-page.jsx
+++ src/pages/resume/resume-page.jsx
@@ -8,7 +8,7 @@ const ResumePage = () =>{
             <h2>Education</h2>
                 <p>2008-2013. Vinnytsia National Technical University.<br />
                 Faculty of Information Technologies and Computer Engineering.<br />
-                Specialty-computer systems and networks.</p> 
+                Specialty - computer systems and networks.</p> 
       
             <h2>Work Experience</h2>
                 <p>Mar.2016 - Feb.2020 Regional organization Vinnytsia
@@ -17,10 +17,9 @@ const ResumePage = () =>{
                 tested applications and system software for user compatibility.
                 Installed, configured, and routinel upgraded software and
                 hardware forend-users. Performed network support tasks for WAN,
-                LAN. Repair and maintenance computer and peripheral equipment.
-                Developed and maintained a website http://smp.vin.ua</p> 
+                LAN. Repair and maintenance computer and peripheral equipment.</p> 
                   
-            <h2>ProfessionalSkills</h2>
+            <h2>Professional Skills</h2>
                 <p>Knowledge of: HTML, CSS, Bootstrap, Material Design
                 Basic Knowledge of: PHP, MySql, JS, JQ, ReactJS
                 Understanding: NPM, Gulp, Git, GitHub
