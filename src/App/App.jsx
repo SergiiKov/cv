0a5240820fa1e069ec605a3ff47871a1c1c5b762
SergiiKov <yamatsuba008@gmail.com>
2020-07-01 13:54:28 +0300

    add mui header

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,24 +10,30 @@ import ProjectsPage from '../pages/projects/projects-page';
 import ResumePage from '../pages/resume/resume-page';
 import LeftArea from '../components/left-area/left-area.component';
 
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import theme from '../components/ui/theme';
+
 function App() {
   return (
     <div>
-      <Header />
-      <div className='grid-container'>
-        <div className='grid-item'>
-        <LeftArea />
-        </div>
-        <div className='grid-item'>
-        <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/contact' component={ContactPage} />
-        <Route path='/projects' component={ProjectsPage} />
-        <Route path='/resume' component={ResumePage} />
-      </Switch>
-        </div>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Header />
+          <div className='grid-container'>
+              <div className='grid-item'>
+                <LeftArea />
+              </div>
+            <div className='grid-item'>
+            <Switch>
+              <Route exact path='/' component={HomePage} />
+              <Route path='/about' component={AboutPage} />
+              <Route path='/contact' component={ContactPage} />
+              <Route path='/projects' component={ProjectsPage} />
+              <Route path='/resume' component={ResumePage} />
+            </Switch>
+            </div>
+          </div>
+      </ThemeProvider>
+     
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,30 +1,92 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './header.style.scss';
 
-const Header = () => {
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+// import './header.style.scss';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  tabContainer: {
+    marginLeft: 'auto'
+  },
+  tab:{
+    fontSize:'1rem'
+  }
+
+}));
+
+export default function Header(props) {
+  const classes = useStyles();
+  const { children, value, index, ...other } = props;
   return (
-    <div className='header'>
-    <Link className='logo-container' to='/'>
-      KOVTUN SERGII
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/about'>
-        About
-      </Link>
-      <Link className='option' to='/resume'>
-        Resume
-      </Link>
-      <Link className='option' to='/projects'>
-        Projects
-      </Link>
-      <Link className='option' to='/contact'>
-        Contact
-      </Link>
-    </div>
-  </div>
-  );
-};
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+         
+          <Link to='/'>
+            <Typography variant="h6" className={classes.title}>
+                KOVTUN SERGII
+            </Typography>
+          </Link>
 
-export default Header;
+          <Tabs className={classes.tabContainer}>
+            <Tab className={classes.tab} label='About' />  
+            <Tab className={classes.tab} label='Resume' />
+            <Tab className={classes.tab} label='Projects' />
+            <Tab className={classes.tab} label='Contact' />
+          </Tabs>
+        
+          <Button color="inherit">Git</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+// const Header = () => {
+//   return (
+//     <div className='header'>
+//     <Link className='logo-container' to='/'>
+//       KOVTUN SERGII
+//     </Link>
+//     <div className='options'>
+//       <Link className='option' to='/about'>
+//         About
+//       </Link>
+//       <Link className='option' to='/resume'>
+//         Resume
+//       </Link>
+//       <Link className='option' to='/projects'>
+//         Projects
+//       </Link>
+//       <Link className='option' to='/contact'>
+//         Contact
+//       </Link>
+//     </div>
+//   </div>
+//   );
+// };
+
+// export default Header;
+
