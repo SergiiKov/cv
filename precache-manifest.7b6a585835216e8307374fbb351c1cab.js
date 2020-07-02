23c10ab9c16008f204a213a169189fc170fb6192
SergiiKov <yamatsuba008@gmail.com>
2020-07-02 09:10:26 +0300

    Updates

---
+++ precache-manifest.7b6a585835216e8307374fbb351c1cab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853309428dbb9f55a330afbe2938c606",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "5c73501510727c151708",
+    "url": "/cv/static/css/main.4a93d177.chunk.css"
+  },
+  {
+    "revision": "c4a9087e470765068c23",
+    "url": "/cv/static/js/2.8018dc5f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cv/static/js/2.8018dc5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c73501510727c151708",
+    "url": "/cv/static/js/main.71fb1346.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "53ef23b9e1ae3a7f8c310175697c6166",
+    "url": "/cv/static/media/MyPhoto.53ef23b9.jpg"
+  }
+]);
