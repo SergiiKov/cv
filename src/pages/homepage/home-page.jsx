a4a678b83abb3aae6221871d06bd2d8537c7dd6c
SergiiKov <yamatsuba008@gmail.com>
2020-07-03 09:44:05 +0300

    add styles fonts v2

--- src/pages/homepage/home-page.jsx
+++ src/pages/homepage/home-page.jsx
@@ -13,13 +13,10 @@ class HomePage extends React.Component {
                 <h2>ABOUT THIS SITE</h2>
                 <h3>A BEAUTIFUL, RESPONSIVE, REACT APP WRITTEN WITH MODERN JAVASCRIPT.</h3>
                 <p>This is react website created to introduce me and my skills in web development (and my skills in english).</p>
-                <p>Source available <a href='https://mdbootstrap.com/docs/jquery/css/colors/'>here.</a></p>
+                <p>Source available <a href='https://github.com/SergiiKov/cv.git'>here.</a></p>
                     <div>
                         <p>It's just a string</p>
-                        <p>This string randomly changes color. Gradient!</p>
-                        <p>This line will disappear after five clicks and appear in ten seconds</p>
-                        <p>This line will disappear after two clicks and reappear in one hour. Do not try to update the site after cliks it will not help!</p>
-                        <p>I said it wouldn't help</p>
+                        <p>This string randomly changes color.</p>
                     </div>
             </div>
             );
