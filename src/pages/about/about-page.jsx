97a0aac5912e0c8cabb797c6d9ab60796f0c9623
SergiiKov <yamatsuba008@gmail.com>
2020-07-08 09:23:49 +0300

    fix bug v2

--- src/pages/about/about-page.jsx
+++ src/pages/about/about-page.jsx
@@ -1,47 +1,95 @@
-import React from 'react';
-import SearchBox from '../../components/serch-box/serch-box.component';
-import Scroll from '../../components/scroll/scroll.component';
-import SkillsList from '../../components/skills-list/skills-list.component';
-import '../about/about-page.styles.scss';
-import { skills } from '../../components/skills';
+import React, {useState} from 'react';
 
-class AboutPage extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            searchField:'',
-            skills: skills 
-        }
-    };
-    
-    handleChange = e => {
-        this.setState({searchField: e.target.value});
+import {  MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Typography from '@material-ui/core/Typography';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+// import './header.style.scss';
+
+const useStyles = makeStyles({
+  cardMargin:{
+    marginTop: '20px',
+    marginLeft: '20px',
+    marginRight: '20px'
+  }
+});
+
+const themeObject = {
+  palette: {
+    primary: {
+      main: '#053f5b',
+    },
+    secondary: {
+      main: '#5e3c6f',
+    },
+    type:'dark'
+  }
+}
+
+const useDarkMode = () =>{
+const [theme, setTheme] = useState(themeObject)
+const { palette: { type } } = theme;
+
+const toggleDarkMode = () => {
+  const updateTheme = {
+    ...theme,
+    palette: {
+      ...theme.palette,
+      type: type === 'light' ? 'dark' : 'light'
     }
-    render(){
-        const { searchField } = this.state;
-        const filterSkills= this.state.skills.filter(skill => 
-            skill.name.toLowerCase().includes(searchField.toLowerCase()))
-        return(
-            <React.Fragment>
-                <div className='about-page'>
-                <h2>ABOUT ME</h2>
-                <p>Hi, I'm Kovtun Sergii a software engineer with over four years IT industry experience, that wants to evolve in the field of web development.</p> 
-                <p>I want to try something new and ready to work hard to learn it.</p>
-                <h3>SOME STATS ABOUT ME</h3>
-                <p>Current age:</p>
-                <p>Current city:</p>
-                <p>Marital status: link</p>
-                        <SearchBox
-                            placeholder='skill' 
-                            handleChange={this.handleChange}
-                        /> 
-                        <SkillsList skills={filterSkills} />
-                    {/* <Scroll></Scroll> */}
-                </div>       
-            </React.Fragment>
-            
-        )
-    }
-};
+  }
+  setTheme(updateTheme)
+}
+return [theme, toggleDarkMode]
+}
+
+const AboutPage = () => {
+  const classes = useStyles();
+  const [theme, toggleDarkMode] = useDarkMode()
+  const themeConfig = createMuiTheme(theme)
+  console.log(theme);
+ 
+  return (
+      <MuiThemeProvider theme={themeConfig}>
+       <React.Fragment>
+   <Card className={classes.cardMargin}>
+       <CardContent>
+       < FormControlLabel control={<Switch onClick={toggleDarkMode}/>} />
+          <Typography variant="h5">
+          ABOUT ME
+          </Typography>
+
+          <Typography variant="body1">
+          Hi, I'm Kovtun Sergii a software engineer with over four years IT industry experience, that wants to evolve in the field of web development.
+          I want to try something new and ready to work hard to learn it.        
+          </Typography>
+          
+          <Typography variant="subtitle1">
+          SOME STATS ABOUT ME
+          </Typography>
+
+          <Typography variant="body1">
+          Current age:
+          </Typography>
+
+          <Typography variant="body1">
+          Current city:
+          </Typography>
+          
+          <Typography variant="body1">
+          Marital status: link
+          </Typography>
+       </CardContent>
+   </Card>
+       </React.Fragment>
+      </MuiThemeProvider>  
+      );
+}
 
 export default AboutPage;
--- src/pages/resume/resume-page.jsx
+++ src/pages/resume/resume-page.jsx
@@ -1,93 +1,61 @@
-import React, {useState} from 'react';
 
-import {  MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import React from 'react';
+import SearchBox from '../../components/serch-box/serch-box.component';
+import Scroll from '../../components/scroll/scroll.component';
+import SkillsList from '../../components/skills-list/skills-list.component';
+import '../about/about-page.styles.scss';
+import { skills } from '../../components/skills';
 
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Typography from '@material-ui/core/Typography';
-
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-
-// import './header.style.scss';
-
-const themeObject = {
-  palette: {
-    primary: {
-      main: '#053f5b',
-    },
-    secondary: {
-      main: '#5e3c6f',
-    },
-    type:'dark'
-  }
-}
-
-const useDarkMode = () =>{
-const [theme, setTheme] = useState(themeObject)
-const { palette: { type } } = theme;
-
-const toggleDarkMode = () => {
-  const updateTheme = {
-    ...theme,
-    palette: {
-      ...theme.palette,
-      type: type === 'light' ? 'dark' : 'light'
+class ResumePage extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            searchField:'',
+            skills: skills 
+        }
+    };
+    
+    handleChange = e => {
+        this.setState({searchField: e.target.value});
     }
-  }
-  setTheme(updateTheme)
-}
-return [theme, toggleDarkMode]
-}
-
-const ResumePage = () => {
-  const [theme, toggleDarkMode] = useDarkMode()
-  const themeConfig = createMuiTheme(theme)
-  console.log(theme);
- 
-  return (
-      <MuiThemeProvider theme={themeConfig}>
-       <React.Fragment>
-   <Card>
-       <CardContent>
-       < FormControlLabel control={<Switch onClick={toggleDarkMode}/>} />
-       <Typography>
-       Education
-       </Typography>
-       <Typography>
-                    2008-2013. Vinnytsia National Technical University.<br />
+    render(){
+        const { searchField } = this.state;
+        const filterSkills= this.state.skills.filter(skill => 
+            skill.name.toLowerCase().includes(searchField.toLowerCase()))
+        return(
+            <React.Fragment>
+                <div className='about-page'>
+                <h3> Education</h3>
+                <p>  2008-2013. Vinnytsia National Technical University.<br />
                     Faculty of Information Technologies and Computer Engineering.<br />
-                    Specialty - computer systems and networks.
-       </Typography>
-       <Typography>
-       Work Experience
-       </Typography>
-       <Typography>
-       Mar.2016 - Feb.2020 Regional organization Vinnytsia
+                    Specialty - computer systems and networks.</p> 
+                <h3>   Work Experience</h3>
+                <p> Mar.2016 - Feb.2020 Regional organization Vinnytsia
                     Information-analytical center as software engineer.
                     Maintenance of  computer systems and networks. Analyzed, and
                     tested applications and system software for user compatibility.
                     Installed, configured, and routinel upgraded software and
                     hardware forend-users. Performed network support tasks for WAN,
-                    LAN. Repair and maintenance computer and peripheral equipment.
-       </Typography>
-       <Typography>
-       Professional Skills
-       </Typography>
-       <Typography>
-       Knowledge of: HTML, CSS, Bootstrap, Material Design
+                    LAN. Repair and maintenance computer and peripheral equipment.</p>
+                <h3> Professional Skills </h3>
+                <p>Knowledge of: HTML, CSS, Bootstrap, Material Design
                     Basic Knowledge of: PHP, MySql, JS, JQ, ReactJS
                     Understanding: NPM, Gulp, Git, GitHub
                     Knowledge of CMS Wordpress, Joomla
                     Knowledge of Adobe Photoshop
-                    Knowledge of English on the Pre-Intermediate level (Read/Write)
-       </Typography>
-       </CardContent>
-   </Card>
-       </React.Fragment>
-      </MuiThemeProvider>  
-      );
-}
+                    Knowledge of English on the Pre-Intermediate level (Read/Write)</p>
+                        
+                        <SearchBox
+                            placeholder='skill' 
+                            handleChange={this.handleChange}
+                        /> 
+                        <SkillsList skills={filterSkills} />
+                    {/* <Scroll></Scroll> */}
+                </div>       
+            </React.Fragment>
+            
+        )
+    }
+};
 
 export default ResumePage;
-
