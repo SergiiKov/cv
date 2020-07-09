27642ca3fc159960db5aab619d7daebb8033b14d
SergiiKov <yamatsuba008@gmail.com>
2020-07-09 12:06:49 +0300

    fix resume page v2

--- src/components/skills.js
+++ src/components/skills.js
@@ -31,14 +31,82 @@ export const skills = [
   },
   {
     id: 6,
+    name: 'Redux',
+    webSite: 'test1',
+    href:"https://redux.js.org/"
+  },
+  {
+    id: 7,
     name: 'Bootstrap',
     webSite: 'test1',
     href:"https://getbootstrap.com/"
   },
   {
-    id: 7,
+    id: 8,
     name: 'MATERIAL-UI',
     webSite: 'test1',
     href:"https://material-ui.com/"
+  },
+  {
+    id: 9,
+    name: 'NPM',
+    webSite: 'test1',
+    href:"https://www.npmjs.com/"
+  },
+  {
+    id: 10,
+    name: 'Webpack',
+    webSite: 'test1',
+    href:"https://webpack.js.org/"
+  },
+  {
+    id: 11,
+    name: 'Gulp',
+    webSite: 'test1',
+    href:"https://gulpjs.com/"
+  }, 
+  {
+    id: 12,
+    name: 'Git',
+    webSite: 'test1',
+    href:"https://git-scm.com/"
+  },
+  {
+    id: 13,
+    name: 'GitHub',
+    webSite: 'test1',
+    href:"https://github.com/"
+  },
+  {
+    id: 14,
+    name: 'PHP',
+    webSite: 'test1',
+    href:"https://www.php.net/"
+  },
+  {
+    id: 15,
+    name: 'MySQL',
+    webSite: 'test1',
+    href:"https://www.mysql.com/"
+  },
+  {
+    id: 16,
+    name: 'WordPress',
+    webSite: 'test1',
+    href:"https://uk.wordpress.org/"
+  },
+  {
+    id: 17,
+    name: 'Joomla!',
+    webSite: 'test1',
+    href:"https://www.joomla.org/"
+  },
+  {
+    id: 18,
+    name: 'Adobe Photoshop',
+    webSite: 'test1',
+    href:"https://www.adobe.com/ua/products/photoshop.html"
   }
+
+
 ];
