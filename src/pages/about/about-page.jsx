9eed1053a5418de4f09d8250e2e1faa94923c854
SergiiKov <yamatsuba008@gmail.com>
2020-07-15 14:23:01 +0300

    fix project page v5

--- src/pages/about/about-page.jsx
+++ src/pages/about/about-page.jsx
@@ -9,17 +9,23 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme=>({
   cardMargin:{
     marginTop: '20px',
     marginLeft: '20px',
-    marginRight: '20px'
+    marginRight: '20px',
+    [theme.breakpoints.down("md")]: {
+      marginTop: '20px',
+      marginLeft: '20px'
+    },
+    [theme.breakpoints.down("xs")]: {
+      margin: '0px'
+    }
   },
   tab: {
-    marginTop: '20px',
-    marginBottom: '10px',
+    marginTop: '20px'
   }
-});
+}));
 
 const themeObject = {
   palette: {
