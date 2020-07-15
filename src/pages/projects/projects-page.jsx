179cf94e4e15db76a689efe70bca5ac383aefab7
SergiiKov <yamatsuba008@gmail.com>
2020-07-15 09:42:40 +0300

    add project page v3

--- src/pages/projects/projects-page.jsx
+++ src/pages/projects/projects-page.jsx
@@ -13,37 +13,36 @@ class ProjectsPage extends React.Component {
              <h2>MY PROJECTS</h2>
             <div className='project'>
                 <h3>smp.vin.ua</h3>
-                <p>Соціальна та молодіжна політика на Вінниччині</p>
-                <a href='http://smp.vin.ua/joomla/' target="_blank" rel="noopener noreferrer">
                     <div className='card-container'>
-                        <img src={require(`../../img/smp.png`)} alt='smp.vin.ua' className='img-district' />
-                            <div className='card-text-container'>
+                    <a href='http://smp.vin.ua/joomla/' target="_blank" rel="noopener noreferrer">
+                        <img src={require(`../../img/smp.png`)} alt='smp.vin.ua' className='img-projects' />
+                        </a> 
+                            <div className='projects-text-container'>
                                 <p>Офіційний сайт. Соціальна та молодіжна політика на Вінниччині</p>
                             </div>   
                     </div>
-                </a>
+                
             </div>
             <div className='project'>
                 <h3>main-page iac</h3>
-                <p>Головна сторінка для сукупності сайтів соціальної політики Вінниччини</p>
-                <a href='' target="_blank" rel="noopener noreferrer">
+                <a href='http://smp.vin.ua/joomla/' target="_blank" rel="noopener noreferrer">
                     <div className='card-container'>
-                        <img src={require(`../../img/main-page.png`)} alt='main-page iac' className='img-district' />
-                            <div className='card-text-container'>
-                                <p>ОКО Інформаційно-аналітичний центр з обслуговування установ соціального захисту населення</p>
+                        <img src={require(`../../img/main-page.png`)} alt='main-page iac' className='img-projects' />
+                            <div className='projects-text-container'>
+                                <p>Головна сторінка для сукупності сайтів соціальної політики Вінниччини. 
+                                    ОКО Інформаційно-аналітичний центр з обслуговування установ соціального захисту населення</p>
                             </div>   
                     </div>
                 </a>
             </div>
             <div className='project'>
                 <h3>VN Create</h3>
-                <p>Сайт візитка розробки сайтів</p>
                 <a href='https://github.com/SergiiKov/vncreate' target="_blank" rel="noopener noreferrer">
                     <div className='card-container'>
-                        <img src={require(`../../img/vncreate-1.png`)} alt='VN Create' className='img-district' />
-                        <img src={require(`../../img/vncreate-2.png`)} alt='VN Create' className='img-district' />
-                            <div className='card-text-container'>
-                                <p>Розробка сайту за бажанням клієнта з унікальним дизайном та можливостями. 
+                        <img src={require(`../../img/vncreate-1.png`)} alt='VN Create' className='img-projects' />
+                        <img src={require(`../../img/vncreate-2.png`)} alt='VN Create' className='img-projects' />
+                            <div className='projects-text-container'>
+                                <p>Сайт візитка. Розробка сайту за бажанням клієнта з унікальним дизайном та можливостями. 
                                     Створення сайту на CMS WordPress, Joomla. 
                                     Реєстрація домену та перенесення на хостинг. Доопрацювання вже існуючого сайту.</p>
                             </div>   
@@ -52,11 +51,10 @@ class ProjectsPage extends React.Component {
             </div>
             <div className='project'>
                 <h3>robot-page</h3>
-                <p>It's just a string</p>
                 <a href='https://github.com/SergiiKov/cards-friends' target="_blank" rel="noopener noreferrer">
                     <div className='card-container'>
-                        <img src={require(`../../img/robot-friends.png`)} alt='robot-friends' className='img-district' />
-                            <div className='card-text-container'>
+                        <img src={require(`../../img/robot-friends.png`)} alt='robot-friends' className='img-projects' />
+                            <div className='projects-text-container'>
                                 <p>React app that searches through an API of random robots</p>
                             </div>   
                     </div>
@@ -64,11 +62,10 @@ class ProjectsPage extends React.Component {
             </div>
             <div className='project'>
                 <h3>Todo List</h3>
-                <p>Todo List app</p>
                 <a href='https://github.com/SergiiKov/My-ToDo-List' target="_blank" rel="noopener noreferrer">
                     <div className='card-container'>
-                        <img src={require(`../../img/todolist.png`)} alt='todolist' className='img-district' />
-                            <div className='card-text-container'>
+                        <img src={require(`../../img/todolist.png`)} alt='todolist' className='img-projects' />
+                            <div className='projects-text-container'>
                                 <p>A simple to-do list application built with React</p>
                             </div>   
                     </div>
